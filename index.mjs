22ffa176b73ad2a707973b399dc3460a049c3340
Jouskaio <jouskaio.me@gmail.com>
2023-09-13 14:34:36 +0200

    Add example

--- index.mjs
+++ index.mjs
@@ -1,8 +1,9 @@
 import express from 'express'
+import employer from "./routes/exemple.mjs";
 
 const app = express()
 
-//app.use('/api', routerLogin);
+//app.use('/api', employer);
 
 app.listen(2000, () => {
   console.log("Server listening on http://localhost:2000")  ;
