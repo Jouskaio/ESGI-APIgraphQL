8ff2a2b004f8dee068e2be03f1d4660f5a533ca1
Jouskaio <jouskaio.me@gmail.com>
2023-09-19 07:48:31 +0200

    Last changes

--- config/postgresql.tsx
+++ config/postgresql.tsx
@@ -1,15 +1,16 @@
-import { } from 'dotenv/config';
+import { config } from 'dotenv';
 import postgresql from 'pg';
 
+config(); // Charge les variables d'environnement du fichier .env
+
 const { Pool } = postgresql;
-const env = process.env
 
 const pool = new Pool({
-  host: env.POSTGRES_HOST,
-  port: env.POSTGRES_PORT,
-  user: env.POSTGRES_USER,
-  password: env.POSTGRES_PASSWORD,
-  database: env.POSTGRES_NAME,
-})
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_NAME,
+});
 
-export default { pool }
+export default { pool };
