18af3b539c9385de337359afb4c74a4d44be4447
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-09-27 09:46:05 +0200

    add qhoeByCode data source method

--- dataSources/shoePSQLDataSource.js
+++ dataSources/shoePSQLDataSource.js
@@ -3,7 +3,7 @@ const { RESTDataSource } = require("apollo-datasource-rest")
 const { pool } = require('../config/postgresql.js');
 
 class ShoePSQLDataSource extends RESTDataSource {
-  async models() {
+  async shoes() {
     let result = await pool.query('SELECT * FROM "SHOE"');
     return result.rows;
   }
@@ -13,8 +13,8 @@ class ShoePSQLDataSource extends RESTDataSource {
     return result.rows;
   }
 
-  async shoeByName(key) {
-    let result = await pool.query('SELECT * FROM "SHOE" WHERE shoe_name = $1', [key]);
+  async shoeByCode(key) {
+    let result = await pool.query('SELECT * FROM "SHOE" WHERE shoe_code = $1', [key]);
     return result.rows;
   }
 }
