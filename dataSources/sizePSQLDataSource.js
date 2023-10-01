2734a2763f280d35b26ff322cb8d0b46a9fde9ea
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-10-01 22:50:33 +0200

    update size data source with addSize mutation

--- dataSources/sizePSQLDataSource.js
+++ dataSources/sizePSQLDataSource.js
@@ -4,6 +4,7 @@ const { pool } = require('../config/postgresql.js');
 
 
 class SizePSQLDataSource extends RESTDataSource {
+  /* Queries */
   async sizes() {
     let result = await pool.query('SELECT * FROM "SIZE"');
     return result.rows;
@@ -16,6 +17,18 @@ class SizePSQLDataSource extends RESTDataSource {
 
   async sizeByName(key) {
     let result = await pool.query('SELECT * FROM "SIZE" WHERE size_name = $1', [key]);
+    return result.rows;
+  }
+
+  /* Mutations */
+
+  async addSize(name) {
+    const insertQuery = 'INSERT INTO "SIZE"(size_name) VALUES($1) RETURNING *';
+    const values = [name];
+
+    /* insert */
+    let result = await pool.query(insertQuery, values);
+
     return result.rows;
   }
 }
