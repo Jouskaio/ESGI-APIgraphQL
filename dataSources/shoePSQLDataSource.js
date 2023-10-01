3134e553eb1d2bca760e50adf16db5358ff44904
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-10-01 22:06:16 +0200

    update shoe datasource with mutation needs review

--- dataSources/shoePSQLDataSource.js
+++ dataSources/shoePSQLDataSource.js
@@ -3,6 +3,7 @@ const { RESTDataSource } = require("apollo-datasource-rest")
 const { pool } = require('../config/postgresql.js');
 
 class ShoePSQLDataSource extends RESTDataSource {
+  /* Queries */
   async shoes() {
     let result = await pool.query('SELECT * FROM "SHOE"');
     return result.rows;
@@ -15,6 +16,40 @@ class ShoePSQLDataSource extends RESTDataSource {
 
   async shoeByCode(key) {
     let result = await pool.query('SELECT * FROM "SHOE" WHERE shoe_code = $1', [key]);
+    return result.rows;
+  }
+
+  /* Mutations */
+
+  async addShoe(
+    pricePurchase,
+    priceSold,
+    datePurchase,
+    dateSold,
+    code,
+    size_key,
+    locationPurchase_key,
+    locationSold_key,
+  ) {
+    const insertQuery =
+        'INSERT INTO "SHOE"(shoe_price_purchase, shoe_price_sold, shoe_date_sold, shoe_date_purchase, shoe_code, size_id, shoe_locpurchase, shoe_locsold) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
+
+    const values = [
+      pricePurchase,
+      priceSold,
+      datePurchase,
+      dateSold,
+      code,
+      size_key,
+      locationPurchase_key,
+      locationSold_key,
+    ];
+
+    /* insert */
+    let result = await pool.query(insertQuery, values);
+
+    console.log(JSON.stringify(result));
+
     return result.rows;
   }
 }
